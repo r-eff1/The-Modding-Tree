d86857d2d571822487c257feabea6fbf2cc636d1
r-eff1 <ivqmref@gmail.com>
2025-06-21 23:36:44 +0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,8 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('p', 11)) gain = gain.times(10)
+	if (hasUpgrade('p', 11)) gain = gain.times(2)
+	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
 	return gain
 }
 
